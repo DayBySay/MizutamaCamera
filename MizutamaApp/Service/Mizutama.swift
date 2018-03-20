5fd8e034561e7925e785ca9466cf37fa922f092a
Takayuki Sei <takayuki_sei@voyagegroup.com>
2018-03-20 09:29:11 +0900

    add model

---
+++ MizutamaApp/Service/Mizutama.swift
@@ -0,0 +1,15 @@
+//
+//  Mizutama.swift
+//  MizutamaApp
+//
+//  Created by 清 貴幸 on 2018/03/20.
+//  Copyright © 2018年 daybysay. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Mizutama {
+    let center: CGPoint
+    let radius: Float
+}
